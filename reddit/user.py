d95600027e03ffc91a462ab118c017d1b6f657ed
Hayk Khachatryan <kh.ha.hayk@gmail.com>
2019-10-01 22:37:17 +0100

    user.py going

---
+++ reddit/user.py
@@ -0,0 +1,28 @@
+import requests
+import time
+
+reddit = 'https://oauth.reddit.com/'
+
+token = 'bearer ' + tokener
+
+headers = {'Authorization': token, 'User-Agent': 'test-my-rest by thirstyfountain'}
+
+
+class User:
+    def __init__(self, u):
+        self.name = u
+
+    def getAbout(self):
+        return requests.get(reddit + '/user/' + self.name + '/about', headers=headers)
+
+    def getKarma(self):
+        return requests.get(reddit + '/api/v1/me/karma', headers=headers)
+
+    def getComments(self):
+        return requests.get(reddit + '/api/v1/me', headers=headers)
+
+    def getTrophies(self):
+        return requests.get(reddit + '/api/v1/user/' + self.name + '/trophies', headers=headers)
+
+    def getCreationDate(self):
+        return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(self.getAbout().json()['data']['created']))
